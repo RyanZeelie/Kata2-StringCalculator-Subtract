9ce401e771bcc0663b9d1802dbca3259b4f7b370
RyanZeelie <ryan.zeelie@kaleidocode.co.za>
2023-08-25 10:58:33 +0800

    Added test cases for delimiters and implementation

--- Calculator/Services/NumberService.cs
+++ Calculator/Services/NumberService.cs
@@ -2,15 +2,41 @@
 {
     public class NumberService : INumberService
     {
-        public string[] _delimiters = new string[] { ",", "\n" };
+        private List<string> _delimiters = new List<string>() { ",", "\n"};
+
+        private const string DelimiterSeperator = "\n";
+        private const string DelimiterIndicator = "##";
+        private string[] DelimiterSurroundings = new string[] { "[", "]", "][" };
 
         public IEnumerable<int> ParseNumbers(string input)
         {
-            var splitNumbers = input.Split(_delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var stringNumbers = GetStringNumbers(input);
 
-            var parsedNumbers = ValidateAndParseNumbers(splitNumbers);
+            var parsedNumbers = ValidateAndParseNumbers(stringNumbers);
 
             return parsedNumbers;
+        }
+
+        private string[] GetStringNumbers(string input)
+        {
+            if (input.StartsWith(DelimiterIndicator))
+            {
+                var splitByNewLine = input.Replace(DelimiterIndicator,string.Empty).Split(DelimiterSeperator, StringSplitOptions.RemoveEmptyEntries);
+
+                SetDelimitiers(splitByNewLine[0].Split(DelimiterSurroundings,StringSplitOptions.RemoveEmptyEntries));
+
+                return splitByNewLine[1].Split(_delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            return input.Split(_delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void SetDelimitiers(string[] delimiters)
+        {
+            foreach (var delimiter in delimiters)
+            {
+                _delimiters.Add(delimiter);
+            }
         }
 
         private IEnumerable<int> ValidateAndParseNumbers(IEnumerable<string> stringOfNumbers)
--- CalculatorTests/NumberServiceTests.cs
+++ CalculatorTests/NumberServiceTests.cs
@@ -45,5 +45,47 @@ namespace CalculatorTests
             // Assert
             Assert.That(expectedResult, Is.EqualTo(result));
         }
+
+        [Test]
+        public void GIVEN_StringOfNumbersSeperatedByCustomDelimiter_WHEN_ParsingNumbers_RETURNS_ListOfNumbersAsInt()
+        {
+            // Arrange
+            var input = "##%\n1%2%3";
+            var expectedResult = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var result = _numberService.ParseNumbers(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void GIVEN_StringOfNumbersSeperatedByMultipleCustomDelimiters_WHEN_ParsingNumbers_RETURNS_ListOfNumbersAsInt()
+        {
+            // Arrange
+            var input = "##[%][;]\n1;2%3";
+            var expectedResult = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var result = _numberService.ParseNumbers(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void GIVEN_StringOfNumbersSeperatedByDelimitierOfAnyLength_WHEN_ParsingNumbers_RETURNS_ListOfNumbersAsInt()
+        {
+            // Arrange
+            var input = "##[***][;;;;]\n1;;;;2***3";
+            var expectedResult = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var result = _numberService.ParseNumbers(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
     }
 }
