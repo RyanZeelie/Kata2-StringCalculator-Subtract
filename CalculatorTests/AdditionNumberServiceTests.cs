6c2f6b59c309c49177a25dd51bd86fc8bdf0a298
RyanZeelie <ryan.zeelie@kaleidocode.co.za>
2023-09-03 14:07:01 +0800

    Added NumberService tests for returning numbers seperated by default delimiters

--- CalculatorTests/AdditionNumberServiceTests.cs
+++ CalculatorTests/AdditionNumberServiceTests.cs
@@ -1,12 +1,32 @@
-﻿namespace CalculatorTests
+﻿using Calculator.Services.Delimiters;
+using Calculator.Services.Numbers;
+
+namespace CalculatorTests
 {
     [TestFixture]
     public class AdditionNumberServiceTests
     {
-        [SetUp] 
-        public void SetUp() 
-        {
+        private INumberService _numberService;
 
+        [SetUp]
+        public void Setup()
+        {
+            var subtractionDelimitierService = new SubtractionDelimiterService();
+            _numberService = new SubtractionNumberService(subtractionDelimitierService);
+        }
+
+        [TestCase("1,2,3")]
+        [TestCase("1\n2\n3")]
+        public void GIVEN_StringOfNumbersSeperatedByDefaultDelimiters_WHEN_ParsingNumbers_RETURNS_ListOfNumbersAsInt(string input)
+        {
+            // Arrange
+            var expectedResult = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var result = _numberService.ParseNumbers(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
         }
     }
 }
