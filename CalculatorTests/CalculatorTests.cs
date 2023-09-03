ba2d9a7b20bfd2d8896901b3dd258129df5cab8d
RyanZeelie <ryan.zeelie@kaleidocode.co.za>
2023-09-03 13:57:57 +0800

    Added tests for subtracting and adding numbers seperated by a new line

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -45,5 +45,33 @@ namespace CalculatorTests
             // Assert
             Assert.That(expectedResult, Is.EqualTo(result));
         }
+
+        [Test]
+        public void GIVEN_StringNumbersSeperatedByNewLine_WHEN_SubtractingStringNumbers_SHOULD_SubtractNumbers()
+        {
+            // Arrange
+            var input = "1\n2\n3";
+            var expectedResult = -6;
+
+            // Act
+            var result = _calculator.Subtract(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void GIVEN_StringNumbersSeperatedByNewLine_WHEN_AddingStringNumbers_SHOULD_AddNumbers()
+        {
+            // Arrange
+            var input = "1\n2\n3";
+            var expectedResult = 6;
+
+            // Act
+            var result = _calculator.Add(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
     }
 }
